751281ac93c79a00692c9b8afcfa5596dca6ec87
Liqiang Su <sudevil1989@gmail.com>
2013-01-25 14:06:58 -0800

    add js file

---
+++ rotate180.js
@@ -0,0 +1,20 @@
+function(){
+	if (typeof jQuery === "undefined") {
+	var script = document.createElement('script');
+	script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
+	script.type = 'text/javascript';document.getElementsByTagName('head')[0].appendChild(script);
+	}
+	var script = document.createElement('script');
+	script.src = 'http://jqueryrotate.googlecode.com/files/jQueryRotateCompressed.2.2.js';
+	script.type = 'text/javascript';document.getElementsByTagName('head')[0].appendChild(script);
+
+	
+	var $ = jQuery;
+	var images = $('img');	
+	if(typeof(images.getRotateAngle()[0]) === 'undefined'){
+		images.rotate(180);		
+	}else{
+		images.rotate(0);
+	}
+	
+}
