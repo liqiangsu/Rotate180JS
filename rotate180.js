bda129b864adbd5b3ea760392c995a4e8f512532
Liqiang Su <sudevil1989@gmail.com>
2013-01-25 23:51:39 -0800

    remove type

--- rotate180.js
+++ rotate180.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded',function(){
 	if (typeof jQuery === "undefined") {
 	var script = document.createElement('script');
 	script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
-	script.type = 'text/javascript';document.getElementsByTagName('head')[0].appendChild(script);
+//	script.type = 'text/javascript';
+	document.getElementsByTagName('head')[0].appendChild(script);
 	}
 	var script = document.createElement('script');
 	script.src = 'http://jqueryrotate.googlecode.com/files/jQueryRotateCompressed.2.2.js';
-	script.type = 'text/javascript';document.getElementsByTagName('head')[0].appendChild(script);
+//	script.type = 'text/javascript';
+	document.getElementsByTagName('head')[0].appendChild(script);
 
 	
 	var $ = jQuery;
