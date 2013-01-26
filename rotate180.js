7af69132791264c7b8871f9b8f9f259d1ce85528
Liqiang Su <sudevil1989@gmail.com>
2013-01-26 00:45:39 -0800

    ...

--- rotate180.js
+++ rotate180.js
@@ -1,4 +1,4 @@
-function(){
+function rotateIt (){
 	if (typeof jQuery === "undefined") {
 	var script = document.createElement('script');
 	script.src = 'http://jqueryjs.googlecode.com/files/jquery-1.2.6.min.js';
